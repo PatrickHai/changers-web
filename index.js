077fecb8d093e36b1a5715f86258ecbfa94bb252
wunier <wunier@wanda.cn>
2016-09-14 17:55:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http')
+
+/*http.createServer(function(req, res){
+  res.write('The very first changers web page!')
+  res.end()
+}).listen(9000)
+*/
+
+var options = {
+  host: 'api.rights.changker.com',
+  path: '/rights/show?id=5',
+  method: 'GET'
+}
+var req = http.request(options, function(response){
+        var str = ''
+        response.setEncoding('utf8')
+        response.on('data', function(chunk){
+          str += chunk
+        })
+        response.on('end', function(){
+          console.log(str)
+        })
+    })
+    req.on('error', function(e){
+      console.log('issue:'+ e.message)
+    })
+    req.end()
